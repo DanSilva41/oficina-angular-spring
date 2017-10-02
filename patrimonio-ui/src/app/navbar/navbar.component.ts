7b5811aa79d3356df4ad11fc75f2804252c63644
Danilo Silva Pereira <dansilva41@hotmail.com>
2017-10-02 12:46:44 -0300

    Utilizando simples table HTML com *ngFor

--- patrimonio-ui/src/app/navbar/navbar.component.ts
+++ patrimonio-ui/src/app/navbar/navbar.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   
-  empresa = 'Teste'
+  empresa = 'Algaworks'
   constructor() { }
 
   ngOnInit() {
